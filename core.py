cfd898c5f596d8ce3f3fe91696da0ec7a57a00f8
j-rivera-almeida <jssrvr27@gmail.com>
2024-06-29 22:59:18 -0500

    use larger model

--- core.py
+++ core.py
@@ -5,7 +5,7 @@ def core(audio_path:str)->str:
   device:str = "cuda"
   compute_type:str = "float16"
 
-  model:Whisper = whisper.load_model(name="models/whisper-tiny.en", device=device, compute_type=compute_type)
+  model:Whisper = whisper.load_model(name="models/whisper-large-v3.en", device=device, compute_type=compute_type)
   results:str = model.transcribe(audio_path=audio_path, temperature=0.0, word_timestamps=False)
 
   # delete model if low on GPU resources
